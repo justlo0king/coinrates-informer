612268d1b55e72f74af0ca6de0e23978e87db39a
justlo0king <info@way.systems>
2021-07-12 05:21:09 +0700

    connections: hooks review

--- src/services/connections/connections.hooks.js
+++ src/services/connections/connections.hooks.js
@@ -3,75 +3,126 @@
 import getConnectionManager from '../../modules/connections';
 import getCoinrateManager from '../../modules/coinrates';
 
+/**
+ * denies external requests
+ * @param {*} hook
+ * @returns
+ */
+const denyExternal = (hook) => {
+  const { provider } = hook.params || {};
+  if (provider) {
+    // only backend is allowed to find records
+    throw new Error('Permission denied');
+  }
+  return hook;
+};
+
+/**
+ * adds createdAt/updatedAt values to new record data
+ * @param {HookContext} hook - feathers hook context
+ * @returns {HookContext} - context with modified data
+ */
+const addCreatedAt = (hook) => {
+  hook.data = hook.data || {};
+  const now = Date.now();
+  hook.data.createdAt = now;
+  hook.data.updatedAt = now;
+  return hook;
+};
+
+/**
+ * cuts "command" from data,
+ * saves it to hook.params to use after patch,
+ * adds updatedAt to patch data
+ * @param {HookContext} hook - feathers hook context
+ * @returns {HookContext} - context with modified data
+ */
+const limitToCommand = (hook) => {
+  hook.data = hook.data || {};
+  const { command } = hook.data || {};
+  // not backend request
+  if (!command || Object.keys(hook.data).length !== 1) {
+    // only 'command' in patch is allowed from external requests
+    //app.error('connections.before.patch: not allowed data: ', hook.data);
+    throw new Error('Not allowed data');
+  }
+  const { name:commandName } = command || {};
+
+  //app.debug('connections.before.patch: data: ', hook.data);
+  if (!commandName || typeof commandName !== 'string') {
+    throw new Error('Not allowed command');
+  }
+
+  const allowedCommands = [ 'coinrates' ];
+
+  if (allowedCommands.indexOf(commandName) == -1) {
+    throw new Error('Command not recognized');
+  }
+
+  hook.params.command = command;
+  delete hook.data.command;
+
+  hook.data.updatedAt = Date.now();
+  return hook;
+};
+
+/**
+ * executes commands saved in hook.params when connection is patched
+ * @param {HookContext} hook - feathers hook context
+ * @returns {HookContext}
+ */
+const executeCommand = (hook) => {
+  const { command } = hook.params || {};
+  const { name:commandName } = command || {};
+  if (!commandName || typeof commandName != 'string') {
+    // there was no command
+    return hook;
+  }
+  if (commandName === 'coinrates') {
+    const { getRates } = getCoinrateManager();
+    const { send } = getConnectionManager();
+    const rates = getRates();
+
+    // result can be array or object, - making sure it is array
+    const resultData = Array.isArray(hook.result) ? hook.result : [ hook.result ];
+    resultData.map((connection) => {
+      //app.debug('connections.after.update: command: ', command);
+      const { id:connectionId } = connection || {};
+      //app.debug('connections.after.update: sending updates, connectionId: ', connectionId);
+      send(connectionId, 'coinrates', rates);
+    });
+  }
+  return hook;
+};
+
 module.exports = {
   before: {
     all: [],
 
-    find: [(hook) => {
-      const { provider } = hook.params || {};
-      if (provider) {
-        // only backend is allowed to find records
-        throw new Error('Permission denied');
-      }
-      return hook;
-    }],
+    find: [
+      denyExternal
+    ],
 
-    get: [(hook) => {
-      const { provider } = hook.params || {};
-      if (provider) {
-        // only backend is allowed to get records
-        throw new Error('Permission denied');
-      }
-      return hook;
-    }],
+    get: [
+      denyExternal
+    ],
 
-    create: [(hook) => {
-      const { provider } = hook.params || {};
-      if (provider) {
-        // only backend is allowed to create records
-        throw new Error('Permission denied');
-      }
+    create: [
+      denyExternal,
+      addCreatedAt
+    ],
 
-      hook.data = hook.data || {};
-      const now = Date.now();
-      hook.data.createdAt = now;
-      hook.data.updatedAt = now;
-      return hook;
-    }],
+    update: [
+      denyExternal
+    ],
 
-    update: [() => {
-      throw new Error('Update not allowed');
-    }],
+    patch: [
+      limitToCommand
+    ],
 
-    patch: [(hook) => {
-      const { provider } = hook.params || {};
-      hook.data = hook.data || {};
-      const { command } = hook.data || {};
-      if (provider) {
-        // not backend request
-        if (!command || Object.keys(hook.data).length !== 1) {
-          // only 'command' in patch is allowed from external requests
-          //app.error('connections.before.patch: not allowed data: ', hook.data);
-          throw new Error('Not allowed data');
-        }
-      }
-      //app.debug('connections.before.patch: data: ', hook.data);
-      if (command) {
-        hook.params.command = command;
-        delete hook.data.command;
-      }
-      hook.data.updatedAt = Date.now();
-      return hook;
-    }],
-
-    remove: [(hook) => {
-      const { provider } = hook.params || {};
-      if (provider) {
-        // only backend is allowed to remove records
-        throw new Error('Permission denied');
-      }
-      return hook;
-    }]
+    remove: [
+      denyExternal
+    ]
   },
 
   after: {
@@ -81,35 +132,7 @@ module.exports = {
     create: [],
     update: [],
     patch: [
-      /**
-       * executes commands when connection is patched
-       * @param {HookContext} hook - feathers hook context
-       * @returns
-       */
-      (hook) => {
-        const { command } = hook.params || {};
-        const { name:commandName } = command || {};
-        if (!commandName || typeof commandName != 'string') {
-          // there was no command
-          return hook;
-        }
-        if (commandName === 'coinrates') {
-          const { getRates } = getCoinrateManager();
-          const { send } = getConnectionManager();
-          const rates = getRates();
-
-          // result can be array or object, - making sure it is array
-          const resultData = Array.isArray(hook.result) ? hook.result : [ hook.result ];
-          resultData.map((connection) => {
-            //app.debug('connections.after.update: command: ', command);
-            const { id:connectionId } = connection || {};
-            //app.debug('connections.after.update: sending updates, connectionId: ', connectionId);
-            send(connectionId, 'coinrates', rates);
-          });
-        }
-
-        return hook;
-      }
+      executeCommand
     ],
     remove: []
   },
